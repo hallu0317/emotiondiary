ca723d47594d8696be9074b457ac7818aab90070
jiwon <hallu0317@gmail.com>
2022-02-18 18:14:10 +0900

    0218 커밋

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RouteTest from "./components/RouteTest";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
 
 function App() {
   return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
